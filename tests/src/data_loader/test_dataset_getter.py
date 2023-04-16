7b6264e9281007c96ac0abb66e08bb08ab6fb7af
menquan <menquan@micron.com>
2023-04-17 00:48:34 +0800

    [chore] clean up absolute path in test case to avoid some paths

--- tests/src/data_loader/test_dataset_getter.py
+++ tests/src/data_loader/test_dataset_getter.py
@@ -4,7 +4,7 @@ from src.data_loader.dataset_getter import DatasetGetter
 
 
 def test_dataset_getter():
-    temp_image_path = Path('/home/hdfsf10n/menquan/bayesian_test/data')
+    temp_image_path = Path('../../bayesian_test/data')
     batch_size = 12
     dataset = DatasetGetter(temp_image_path, batch_size).get()
     print(dataset)
