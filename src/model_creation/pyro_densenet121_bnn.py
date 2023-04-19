[fix] fixed pyro plate and softmax issues in pyro BNNs

src/model_creation/pyro_densenet121_bnn.py
@@ -19,14 +19,12 @@ class PyroDensenet121Bnn(pyro.nn.PyroModule):
 
     def forward(self, x, y=None):
         logits = self._net(x)
-        with pyro.plate("data_plate", x.shape[0]):
-            pyro.sample("data", Categorical(logits=logits).to_event(), obs=y)
+        pyro.sample("data", Categorical(logits=logits).to_event(), obs=y)
         return logits
 
     def model(self, x, y=None):
         logits = self._net(x)
-        with pyro.plate("data_plate", x.shape[0]):
-            pyro.sample("data", Categorical(logits=logits).to_event(), obs=y)
+        pyro.sample("data", Categorical(logits=logits).to_event(), obs=y)
         return logits
 
     def _get_guide(self):
@@ -52,7 +50,7 @@ class PyroDensenet121Bnn(pyro.nn.PyroModule):
                 bias=True,
             )
         )
-        net.add_module('logsoftmax', nn.LogSoftmax(dim=1))
+        net.add_module('softmax', nn.Softmax(dim=-1))
         net.classifier.requires_grad = True
         net.logsoftmax.requires_grad = True
         net.to(device)

src/model_creation/pyro_miniresnet_bnn.py
@@ -20,14 +20,12 @@ class PyroMiniresnetBnn(pyro.nn.PyroModule):
 
     def forward(self, x, y=None):
         logits = self._resnet(x)
-        with pyro.plate("data_plate", x.shape[0]):
-            pyro.sample("data", Categorical(logits=logits).to_event(), obs=y)
+        pyro.sample("data", Categorical(logits=logits).to_event(), obs=y)
         return logits
 
     def model(self, x, y=None):
         logits = self._resnet(x)
-        with pyro.plate("data_plate", x.shape[0]):
-            pyro.sample("data", Categorical(logits=logits).to_event(), obs=y)
+        pyro.sample("data", Categorical(logits=logits).to_event(), obs=y)
         return logits
 
     def _get_guide(self):
@@ -42,7 +40,7 @@ class PyroMiniresnetBnn(pyro.nn.PyroModule):
         resnet = ResNet(
             BasicBlock, [1, 1, 1, 1], dataset.num_classes
         )
-        resnet.add_module('logsoftmax', nn.LogSoftmax(dim=1))
+        resnet.add_module('softmax', nn.Softmax(dim=-1))
         resnet.to(device)
         return resnet
 

src/model_creation/pyro_resnet18_bnn.py
@@ -19,14 +19,12 @@ class PyroResnet18Bnn(pyro.nn.PyroModule):
 
     def forward(self, x, y=None):
         logits = self._resnet(x)
-        with pyro.plate("data_plate", x.shape[0]):
-            pyro.sample("data", Categorical(logits=logits).to_event(), obs=y)
+        pyro.sample("data", Categorical(logits=logits).to_event(), obs=y)
         return logits
 
     def model(self, x, y=None):
         logits = self._resnet(x)
-        with pyro.plate("data_plate", x.shape[0]):
-            pyro.sample("data", Categorical(logits=logits).to_event(), obs=y)
+        pyro.sample("data", Categorical(logits=logits).to_event(), obs=y)
         return logits
 
     def _get_guide(self):
@@ -52,9 +50,9 @@ class PyroResnet18Bnn(pyro.nn.PyroModule):
                 bias=True,
             )
         )
-        resnet.add_module('logsoftmax', nn.LogSoftmax(dim=1))
+        resnet.add_module('softmax', nn.Softmax(dim=-1))
         resnet.fc.requires_grad = True
-        resnet.logsoftmax.requires_grad = True
+        resnet.softmax.requires_grad = True
         resnet.to(device)
         return resnet
 